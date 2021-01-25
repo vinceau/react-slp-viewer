8e809302508741e7e06359262f7c5ae274abe3fc
Vince Au <vinceau09@gmail.com>
2021-01-26 09:24:46 +1100

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,10 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
 import sass from "rollup-plugin-sass";
 import image from "@rollup/plugin-image";
 
 import pkg from "./package.json";
-
-const minifyExtension = (pathToFile) => pathToFile.replace(/\.js$/, ".min.js");
 
 export default {
   input: "src/index.ts",
@@ -19,25 +16,15 @@ export default {
       sourcemap: true,
     },
     {
-      file: minifyExtension(pkg.main),
-      format: "cjs",
-      sourcemap: false,
-    },
-    {
       file: pkg.module,
       format: "esm",
       sourcemap: true,
     },
   ],
   plugins: [
-    terser({
-      include: [/^.+\.min\.js$/],
-    }),
     peerDepsExternal(),
     resolve(),
-    commonjs({
-      exclude: ["node_modules/react-svg-unique-id/**/*.js"],
-    }),
+    commonjs(),
     typescript({ useTsconfigDeclarationDir: true }),
     image(),
     sass({
